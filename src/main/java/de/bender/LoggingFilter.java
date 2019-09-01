1ab3d67ff6bbe3453c338255925f6e792df9cd23
Stefan Schoeffmann <stefan.schoeffmann@bmw.de>
2019-09-01 12:24:16 +0200

    Final adjustments to naming and logging

--- src/main/java/de/bender/LoggingFilter.java
+++ src/main/java/de/bender/LoggingFilter.java
@@ -4,12 +4,25 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Provider
 public class LoggingFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        requestContext.getCookies()
-                .forEach((key, value) -> System.out.println(key + ": " + value));
+        final String cookies = requestContext.getCookies()
+                .entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue().toString())
+                .collect(Collectors.joining("\n\t\t"));
+
+        final String headers = requestContext.getHeaders()
+                .entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .collect(Collectors.joining("\n\t\t"));
+
+        System.out.println("Request: " +
+                "\n\tMethod: " + requestContext.getMethod() +
+                "\n\tCookies: " + cookies +
+                "\n\tHeaders: " + headers);
     }
 }
--- src/main/java/de/bender/ping/boundary/PingResource.java
+++ src/main/java/de/bender/ping/boundary/PingResource.java
@@ -6,7 +6,9 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 @Path("ping")
@@ -20,6 +22,10 @@ public class PingResource {
                 .add("id", 1)
                 .add("name", "FooBar")
                 .build();
-        return Response.ok(build).build();
+
+        return Response
+                .ok(build)
+                .cookie(new NewCookie("CORS-Cookie", "CookieValue for CORS-Tests"))
+                .build();
     }
 }
